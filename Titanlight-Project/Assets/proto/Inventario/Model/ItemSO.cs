e00a3bd20abec93186cbc5dd3d1f2f9a535bd1cf
OaKarvalho <OaKarvalho@users.noreply.github.com>
2025-06-23 06:50:28 -0300

    agora da pra buildar
    sssa

--- Titanlight-Project/Assets/proto/Inventario/Model/ItemSO.cs
+++ Titanlight-Project/Assets/proto/Inventario/Model/ItemSO.cs
@@ -1,29 +1,24 @@
-using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using UnityEditor.Compilation;
 using UnityEngine;
 
-    
-[CreateAssetMenu]    
+[CreateAssetMenu]
 public class ItemSO : ScriptableObject
-    
 {
     [field: SerializeField]
     public bool IsStackable { get; set; } //determina se é empilhavel ou nao
+
     public int ID => GetInstanceID(); //ID do item
-    
-    [field: SerializeField]      
+
+    [field: SerializeField]
     public int MaxStackSize { get; set; } = 1;//valor maximo do item empilhavel
 
     [field: SerializeField]
     public int Price { get; set; } = 1;//valor do venda do item
-        
+
     [field: SerializeField]
     public string Name { get; set; }//nome do item
-                                    //
-    [field: SerializeField] 
+
+    [field: SerializeField]
     public Sprite ItemImage { get; set; }//imagem do item
-
 }
-
